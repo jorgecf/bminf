c0c7f8095a3ceb8da60c6af1dad5797031a00350
Alejandro Martin <Alejandro Martin>
2017-03-30 17:46:24 +0200

    Combined comentado

--- bmi-p3-01/src/es/uam/eps/bmi/search/CombinedEngine.java
+++ bmi-p3-01/src/es/uam/eps/bmi/search/CombinedEngine.java
@@ -7,16 +7,23 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
-import es.uam.eps.bmi.search.graph.PageRank;
-import es.uam.eps.bmi.search.index.AbstractIndex;
 import es.uam.eps.bmi.search.index.DocumentMap;
-import es.uam.eps.bmi.search.index.Index;
 import es.uam.eps.bmi.search.index.impl.PositionalIndexBuilderImpl;
 import es.uam.eps.bmi.search.index.impl.PositionalIndexImpl;
 import es.uam.eps.bmi.search.ranking.SearchRanking;
 import es.uam.eps.bmi.search.ranking.SearchRankingDoc;
 import es.uam.eps.bmi.search.ranking.impl.RankingImpl;
 
+/**
+ * Clase CombinedEngine, para calcular un ranking de documentos a partir
+ * de una lista de SearchEngines. Los scores de cada SearchEngine,
+ * seran normalizados y ponderados para obtener una puntuacion general
+ * y asi obtener un ranking coherente.
+ * 
+ * @author Jorge Cifuentes
+ * @author Alejandro Martin
+ *
+ */
 public class CombinedEngine implements SearchEngine {
 	
 	private SearchEngine[] seArr;
@@ -38,21 +45,34 @@ public class CombinedEngine implements SearchEngine {
 	@Override
 	public SearchRanking search(String query, int cutoff) throws IOException {
 		
+		// Array de rankings correspondientes a los SearchEngines
 		SearchRanking[] srArr = new SearchRanking[this.weights.length];
+		
+		// Hash de docId con lista de scores ya normalizados
 		HashMap<Integer, ArrayList<Double>> ranking = new HashMap<Integer, ArrayList<Double>> ();
+		
+		// Hash para docIds de nuestro nuevo indice
 		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		
+		// Hash de docId con su score final
 		HashMap<Integer, Double> hashNorm = new HashMap<Integer, Double>();
 		
+		// Obtenemos los rankings de todos los SearchEngines,
+		// con todos los documentos
 		for (int i = 0; i < this.weights.length; i ++) {
 				srArr[i] = seArr[i].search(query, Integer.MAX_VALUE);
 		}
 		
+		// Min-max
+		
+		// Inicializamos valores de los arrays
 		double[] max = new double[this.weights.length];
 		double[] min = new double[this.weights.length];
 		Arrays.fill(min, 0);
 		Arrays.fill(min, Double.MAX_VALUE);
 		int docID = 0;
 		
+		// Obtenemos los minimos y los maximos de cada ranking
 		for (int i = 0; i < srArr.length; i ++) {
 			for (SearchRankingDoc result : srArr[i]) {
 				if (result.getScore() > max[i])
@@ -66,6 +86,8 @@ public class CombinedEngine implements SearchEngine {
 			}
 		}
 		
+		// Introducimos los scores normalizados en la lista de cada
+		// documento del ranking
 		for (int i = 0; i < srArr.length; i ++) {
 			for (SearchRankingDoc result : srArr[i]) {
 				if (ranking.containsKey(hm.get(result.getPath()))){
@@ -84,6 +106,7 @@ public class CombinedEngine implements SearchEngine {
 		
 		Iterator<Entry<Integer, ArrayList<Double>>> it = ranking.entrySet().iterator();
 		
+		// Obtenemos el score total de cada documento
 		while (it.hasNext()) {
 			Entry<Integer, ArrayList<Double>> res = it.next();
 			
@@ -100,6 +123,7 @@ public class CombinedEngine implements SearchEngine {
 		
 		this.dm = new PositionalIndexImpl("index/combined");
 		
+		// Introducimos en el ranking los scores obtenidos
 		RankingImpl rankingNorm = new RankingImpl(this.dm, cutoff);
 		Iterator<Entry<Integer, Double>> ite = hashNorm.entrySet().iterator();
 		while (ite.hasNext()) {
