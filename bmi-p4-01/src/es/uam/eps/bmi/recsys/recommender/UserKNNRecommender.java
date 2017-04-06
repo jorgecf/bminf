b92db925cf372ccfd64c7453b42d014bc4a05844
Jorge Cifuentes <jorge.cifuentes95@gmail.com>
2017-04-06 18:26:18 +0200

    Empezado user knn.

--- bmi-p4-01/src/es/uam/eps/bmi/recsys/recommender/UserKNNRecommender.java
+++ bmi-p4-01/src/es/uam/eps/bmi/recsys/recommender/UserKNNRecommender.java
@@ -1,19 +1,72 @@
 package es.uam.eps.bmi.recsys.recommender;
 
 import es.uam.eps.bmi.recsys.data.Ratings;
+import es.uam.eps.bmi.recsys.data.RatingsImpl;
+import es.uam.eps.bmi.recsys.ranking.Ranking;
+import es.uam.eps.bmi.recsys.ranking.RankingElement;
+import es.uam.eps.bmi.recsys.ranking.RankingImpl;
 import es.uam.eps.bmi.recsys.recommender.similarity.Similarity;
 
 public class UserKNNRecommender extends AbstractRecommender {
 
-	public UserKNNRecommender(Ratings ratings, Similarity similarity, int k) {
-		// TODO Auto-generated constructor stub
+	// private Ratings ratings;
+	// private RankingImpl rank;
+
+	public UserKNNRecommender(Ratings ratings, Similarity sim, int k) {
 		super(ratings);
+
+		// this.ratings=ratings;
+
+		for (Integer user : ratings.getUsers()) {
+			for (Integer item : ratings.getItems()) {
+
+				// Si no hay rating lo simulamos
+				if (this.ratings.getRating(user, item) == null) {
+
+					// Sumatorio de sim entre user actual y otros users que
+					// han valorado el item
+					Double acc = 0.0;
+
+					for (Integer vuser : ratings.getUsers()) {
+						Double r = this.ratings.getRating(user, item);
+						if (r == null) {
+							acc = sim.sim(user, vuser) * r;
+						}
+					}
+
+					this.ratings.rate(user, item, acc);
+				}
+
+			}
+		}
+
+		// knn
+		Ratings r2 = new RatingsImpl();
+
+		for (Integer user : ratings.getUsers()) {
+
+			Ranking rank = new RankingImpl(k);
+			for (Integer item : ratings.getItems(user)) {
+				rank.add(item, this.ratings.getRating(user, item)); // k mejores
+			}
+
+			for (RankingElement re : rank) {
+				r2.rate(user, re.getID(), re.getScore());
+			}
+		}
+
 	}
 
 	@Override
 	public double score(int user, int item) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		Double r = this.ratings.getRating(user, item);
+		if (r == null)
+			return 0.0;
+		else
+			return r; // TODO simplif con average
+
+		// return this.rank.
 	}
 
 }
--- bmi-p4-01/src/es/uam/eps/bmi/recsys/test/Test.java
+++ bmi-p4-01/src/es/uam/eps/bmi/recsys/test/Test.java
@@ -36,10 +36,10 @@ public class Test {
     public static void main (String a[]) throws FileNotFoundException {
         System.out.println("=========================");
         System.out.println("Testing MovieLens \"latest-small\" dataset");
-        testDataset("data/ratings.csv", "data/tags.csv", ",", new IntParser(), 35, 1176);
+        testDataset("data/ratings.csv", "data/tags.csv", ",", new StringParser(), 35, 1176);
         System.out.println("=========================");
         System.out.println("Testing MovieLens HetRec dataset");
-        testDataset("data/user_ratedmovies.dat", "data/movie_tags.dat", "\t", new StringParser(), 894, 993);
+        testDataset("data/user_ratedmovies.dat", "data/movie_tags.dat", "\t", new IntParser(), 894, 993);
     }
     
     static <F>void testDataset(String ratingsFile, String featuresFile, String separator, Parser<F> featureParser, int user, int item) 
@@ -50,11 +50,11 @@ public class Test {
         double threshold = 4;
 
         Ratings ratings = new RatingsImpl(ratingsFile, separator);
-        Features<String> features = new FeaturesImpl<String>(featuresFile, separator, new StringParser());
+        Features<F> features = new FeaturesImpl<F>(featuresFile, separator, featureParser);
         
-     //   testData(ratings, features, user, item);
+        testData(ratings, features, user, item);
         
-      //  testRecommenders(ratings, features, k, n, 3, 4);
+        testRecommenders(ratings, features, k, n, 3, 4);
 
         Ratings folds[] = ratings.randomSplit(0.8);
         Ratings train = folds[0];
@@ -99,7 +99,6 @@ public class Test {
         testRecommender(new CentroidRecommender<F>(ratings, new CosineFeatureSimilarity<F>(features)), n, nUsers, nItems);
         Timer.reset();
         testRecommender(new ItemNNRecommender(ratings, new JaccardFeatureSimilarity<F>(features)), n, nUsers, nItems);
-
     }
 
     static <U extends Comparable<U>,I extends Comparable<I>,F> void evaluateRecommenders(Ratings ratings, Features<F> features, int k, int n, Metric metrics[]) {
@@ -117,7 +116,6 @@ public class Test {
         evaluateRecommender(new CentroidRecommender<F>(ratings, new CosineFeatureSimilarity<F>(features)), n, metrics);
         Timer.reset();
         evaluateRecommender(new ItemNNRecommender(ratings, new JaccardFeatureSimilarity<F>(features)), n, metrics);
-
     }
 
     static <U,I extends Comparable<I>> void testRecommender(Recommender recommender, int n, int nUsers, int nItems) throws FileNotFoundException {
