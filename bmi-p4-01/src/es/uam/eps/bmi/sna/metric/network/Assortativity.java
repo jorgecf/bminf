98d8688475afbe044a72d94b196eac60555abe2e
Jorge Cifuentes <Jorge.cifuentes95@gmail.com>
2017-04-25 20:09:15 +0200

    Asortatividad.

--- bmi-p4-01/src/es/uam/eps/bmi/sna/metric/network/Assortativity.java
+++ bmi-p4-01/src/es/uam/eps/bmi/sna/metric/network/Assortativity.java
@@ -3,14 +3,39 @@ package es.uam.eps.bmi.sna.metric.network;
 import es.uam.eps.bmi.sna.metric.GlobalMetric;
 import es.uam.eps.bmi.sna.structure.UndirectedSocialNetwork;
 
-public class Assortativity<U> implements GlobalMetric<U>{
+public class Assortativity<U> implements GlobalMetric<U> {
 
 	@Override
 	public double compute(UndirectedSocialNetwork<U> network) {
-	
-		
-		
-		return 0;
+
+		int m = network.nEdges();
+
+		double grads2 = 0.0;
+		double grads3 = 0.0;
+		double gradsEdges = 0.0;
+
+		for (U user : network.getUsers()) {
+			// (sumatorio g(U) ^ 2 ) ^2
+			// el grado es el numero de edges en los que participa
+			grads2 = grads2 + Math.pow(network.getContacts(user).size(), 2);
+
+			// sumatorio g(U) ^ 3
+			grads3 = grads3 + Math.pow(network.getContacts(user).size(), 3);
+
+			for (U user2 : network.getContacts(user)) {
+				gradsEdges = gradsEdges + (network.getContacts(user).size() * network.getContacts(user2).size());
+			}
+		}
+
+		grads2 = Math.pow(grads2, 2);
+		gradsEdges /= 2; // eliminamos arcos repetidos
+
+		return (double) (4 * m * gradsEdges - grads2) / (2 * m * grads3 - grads2);
+	}
+
+	@Override
+	public String toString() {
+		return "Assortativity";
 	}
 
 }
--- bmi-p4-01/src/es/uam/eps/bmi/sna/test/Test.java
+++ bmi-p4-01/src/es/uam/eps/bmi/sna/test/Test.java
@@ -27,12 +27,12 @@ import java.io.FileNotFoundException;
 public class Test {
     public static void main (String a[]) throws FileNotFoundException {
         testNetwork("graph/small1.csv", ",", new IntParser(), 5, 6, 4);
-        testNetwork("graph/small2.csv", ",", new IntParser(), 5, 3, 5);
+      testNetwork("graph/small2.csv", ",", new IntParser(), 5, 3, 5);
         testNetwork("graph/small3.csv", ",", new StringParser(), 5, "a", "b");
-        testNetwork("graph/fb.csv", ",", new IntParser(), 5, 9, 3);
+     /*       testNetwork("graph/fb.csv", ",", new IntParser(), 5, 9, 3);
         testNetwork("graph/twitter.csv", ",", new StringParser(), 5, "el_pais", "ElviraLindo");
         testNetwork("graph/barabasi.csv", ",", new IntParser(), 5, 1, 2);
-        testNetwork("graph/erdos.csv", ",", new IntParser(), 5, 1, 2);
+        testNetwork("graph/erdos.csv", ",", new IntParser(), 5, 1, 2);*/
     }
     
     static <U extends Comparable<U>>void testNetwork(String graphFile, String separator, Parser<U> parser, int topK, U u, U v) throws FileNotFoundException {
@@ -54,7 +54,7 @@ public class Test {
         System.out.println("-------------------------");
         testMetric(new ClusteringCoefficient<U>(), network);
    //     testMetric(new AvgUserMetric<U>(new UserClusteringCoefficient<U>()), network);
- //       testMetric(new Assortativity<U>(), network);
+        testMetric(new Assortativity<U>(), network);
     }
     
     static <T extends Comparable<T>,U>void testMetric(LocalMetric<T,U> metric, UndirectedSocialNetwork<U> network, T example) {
